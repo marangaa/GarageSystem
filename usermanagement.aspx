077d6e9bc02ebc3f10c885646ba5e23e65a47a8e
Maranga <rchdmaranga@gmail.com>
2023-05-09 12:19:26 +0300

    minor changes to usernanagement

--- usermanagement.aspx
+++ usermanagement.aspx
@@ -1,15 +1,56 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="usermanagement.aspx.cs" Inherits="GarageSystem.usermanagement" %>
 
 <!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
+<html>
+<head>
+    <title>User Management</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            text-align: left;
+            padding: 8px;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .btn {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            padding: 8px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 14px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .btn.edit {
+            background-color: #2196F3;
+        }
+        .btn.delete {
+            background-color: #f44336;
+        }
+    </style>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+    <h1>User Management</h1>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="UserId"
+        OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing">
+        <Columns>
+            <asp:BoundField DataField="UserId" HeaderText="User ID" ReadOnly="True" />
+            <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+            <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+            <asp:BoundField DataField="Email" HeaderText="Email" />
+            <asp:BoundField DataField="Mobile" HeaderText="Mobile" />
+            <asp:CommandField ShowEditButton="True" />
+            <asp:CommandField ShowDeleteButton="True" />
+        </Columns>
+    </asp:GridView>
+    <a href="AddUser.aspx" class="btn">Add User</a>
 </body>
 </html>
--- usermanagement.aspx.cs
+++ usermanagement.aspx.cs
@@ -1,17 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace GarageSystem
 {
-    public partial class usermanagement : System.Web.UI.Page
+    public partial class usermanagement : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindUsers();
+            }
+        }
 
+        private void BindUsers()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT UserId, FirstName, LastName, Email, Mobile FROM Users";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    DataTable users = new DataTable();
+                    adapter.Fill(users);
+                    UserList.DataSource = users;
+                    UserList.DataKeyNames = new string[] { "UserId" };
+                    UserList.DataBind();
+                }
+            }
+        }
+
+        protected void UserList_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userId = Convert.ToInt32(UserList.DataKeys[e.RowIndex].Values[0]);
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Users WHERE UserId = @UserId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            BindUsers();
         }
     }
 }
